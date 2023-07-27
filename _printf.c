d98c91383ea66b14700c2fd5a1ba664df102561e
MoroccoBS <Chouaibwidadipur@gmail.com>
2023-07-27 22:33:47 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -11,29 +11,38 @@ int _printf(const char *const format, ...)
 
 	va_list args;
 	int i = 0, j, len = 0;
+	int found_match = 0;
 
 	va_start(args, format);
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	{
 		return (-1);
+	}
 
-Here:
 	while (format[i] != '\0')
 	{
-		j = 13;
-		while (j >= 0)
+		for (j = 12; j >= 0; j--)
 		{
 			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
 			{
 				len += m[j].f(args);
 				i = i + 2;
-				goto Here;
+				found_match = 1;
+				break;
 			}
-			j--;
 		}
+
+		if (found_match)
+		{
+			found_match = 0;
+			continue;
+		}
+
 		_putchar(format[i]);
 		len++;
 		i++;
 	}
+
 	va_end(args);
 	return (len);
 }
